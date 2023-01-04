5665b1ea38d7f1ccf0db110a94992193017114c1
Marek Wal <walmarek1987@gmail.com>
2023-01-04 19:48:28 +0100

    button component functionality moved to input

--- src/Button/index.js
+++ src/Button/index.js
@@ -1,16 +1,7 @@
 import "./style.css";
 
-const Button = () => (
-    <button className="button">
-        <a
-          className="button__link"
-          href="https://www.nbp.pl/home.aspx?f=/kursy/kursya.html"
-          title="Currency rates on NBP website"
-          alt="link for avarege currency rates - NBP website"
-        >
-          Check avarage exchange rates - NBP
-        </a>
-      </button>
-);
+const Button = () => {
+  return <button className="button">Convert</button>;
+};
 
 export default Button;
