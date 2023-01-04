9a8e28ed6e8ab3697c0a542937750ac4fc448c89
Marek Wal <walmarek1987@gmail.com>
2023-01-04 21:19:02 +0100

    final functionality added to App.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
+import { useState } from "react";
 import Button from "./Button";
-import Select from "./Select";
 import Label from "./Label";
 import Fieldset from "./Fieldset";
-import Input from "./Input";
 import Form from "./Form";
+import Section from "./Section";
 
 function App() {
+  const [fromCurrencyRate, setFromCurrencyRate] = useState("4.685");
+  const [toCurrencyRate, setToCurrencyRate] = useState("1");
+  const [result, setResult] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const calculateResult = () => {
+    return (amount * fromCurrencyRate) / toCurrencyRate;
+  };
+
+  const onFormSubmit = (event) => {
+    event.preventDefault();
+    setResult(calculateResult().toFixed(2));
+  };
+
   return (
     <div className="container">
-      <Form>
-        <Fieldset>
-          <Label>
-            <Input />
-            <Select />
-          </Label>
-          <Label>
-            <Input />
-            <Select />
-          </Label>
-        </Fieldset>
-      </Form>
-      <Button />
+      <Form
+        onFormSubmit={onFormSubmit}
+        body={
+          <Fieldset
+            input={
+              <Label
+                title="From:"
+                placeholder="Enter amount"
+                amount={amount}
+                currencyRate={fromCurrencyRate}
+                onChangeAmount={(e) => setAmount(e.target.value)}
+                onChangeCurrencyRate={(e) =>
+                  setFromCurrencyRate(e.target.value)
+                }
+              />
+            }
+            output={
+              <Label
+                title="To:"
+                placeholder="Result"
+                amount={result}
+                currencyRate={toCurrencyRate}
+                onChangeAmount={(e) => setResult(e.target.value)}
+                onChangeCurrencyRate={(e) => setToCurrencyRate(e.target.value)}
+              />
+            }
+            button={<Button />}
+          />
+        }
+      />
+      <Section />
     </div>
   );
 }
