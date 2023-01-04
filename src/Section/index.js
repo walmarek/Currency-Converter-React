29f565565550e77f4f37ff6a5d99fff138bede51
Marek Wal <walmarek1987@gmail.com>
2023-01-04 21:06:57 +0100

    bolded font added

--- src/Section/index.js
+++ src/Section/index.js
@@ -4,14 +4,14 @@ const Section = () => {
   return (
     <section className="section"> 
       <div className="section__link">
-        Rates updated on 31/12/2022.&emsp;
+        Rates updated on 31/12/2022&emsp;
         <a
           className="link"
           href="https://www.nbp.pl/home.aspx?f=/kursy/kursya.html"
           title="Avarage currency exchange rates NBP"
           alt="NBP website link exchange rates"
         >
-          Check avarage exchange rates NBP - link
+        Check avarage exchange rates NBP - <strong>link</strong>
         </a>
       </div>
     </section>
