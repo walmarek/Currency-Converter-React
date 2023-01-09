12586b589f041d0694410c8039335aef3756af5c
Marek Wal <walmarek1987@gmail.com>
2023-01-09 16:38:24 +0100

    Options component created

---
+++ src/Options/index.js
@@ -0,0 +1,9 @@
+const options = [
+    { id: "1", value: "1", name: "PLN" },
+    { id: "2", value: "4.435", name: "USD" },
+    { id: "3", value: "4.685", name: "EUR" },
+    { id: "4", value: "5.432", name: "GBP" },
+    { id: "5", value: "4.747", name: "CHF" },
+  ];
+
+  export default options;
