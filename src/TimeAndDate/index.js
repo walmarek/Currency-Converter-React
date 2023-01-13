e1e21938197c926ae2f8215b5849b371d258cd27
Marek Wal <walmarek1987@gmail.com>
2023-01-13 11:19:45 +0100

    TimeAndDate component created

---
+++ src/TimeAndDate/index.js
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+import "./style.css";
+
+const TimeAndDate = () => {
+  const [date, setDate] = useState(new Date());
+
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setDate(new Date());
+    }, 1000);
+
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+
+  const getDate = () =>
+    date.toLocaleString("en-EN", {
+      weekday: "long",
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+      hour: "2-digit",
+      minute: "2-digit",
+      second: "2-digit",
+    });
+
+  return <div className="date">{getDate(date)}</div>;
+};
+
+export default TimeAndDate;
