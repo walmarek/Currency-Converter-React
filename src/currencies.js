2da058451ba06df9f957014a9fc2e9608991a20f
Marek Wal <walmarek1987@gmail.com>
2023-02-06 12:21:36 +0100

    currencies.js deleted

--- src/currencies.js
+++
@@ -1,9 +0,0 @@
-const currencies = [
-  { id: "1", value: "1", name: "PLN" },
-  { id: "2", value: "4.435", name: "USD" },
-  { id: "3", value: "4.685", name: "EUR" },
-  { id: "4", value: "5.432", name: "GBP" },
-  { id: "5", value: "4.747", name: "CHF" },
-];
-
-export default currencies;
