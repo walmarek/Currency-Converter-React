a1b7fd13a80174ed67f1d5fa4c76224e1b4360ce
Marek Wal <walmarek1987@gmail.com>
2023-02-06 12:22:27 +0100

    message changed

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,18 +1,15 @@
 import { Container, Box } from "./styled";
 
+
+
+
 const Footer = () => {
+  
+
   return (
     <Container>
       <Box>
-        Rates updated on 31/12/2022&emsp;
-        <Box
-          as="a"
-          href="https://www.nbp.pl/home.aspx?f=/kursy/kursya.html"
-          title="Avarage currency exchange rates NBP"
-          alt="NBP website link exchange rates"
-        >
-          Check avarage exchange rates NBP - <strong>link</strong>
-        </Box>
+        <Box>Exchange rates updated live from European Central Bank.</Box>
       </Box>
     </Container>
   );
