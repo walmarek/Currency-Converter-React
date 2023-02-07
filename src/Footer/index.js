59ec220b2fcfbbd310173b3828659c0e6fe43901
Marek Wal <walmarek1987@gmail.com>
2023-02-07 11:09:44 +0100

    date added

--- src/Footer/index.js
+++ src/Footer/index.js
@@ -1,15 +1,14 @@
+import useCurrencyOptions from "../useCurrencyOptions";
 import { Container, Box } from "./styled";
 
-
-
-
 const Footer = () => {
-  
-
+  const { date } = useCurrencyOptions();
   return (
     <Container>
       <Box>
-        <Box>Exchange rates updated live from European Central Bank.</Box>
+        <Box>
+          Exchange rates updated: {`${date}`} from European Central Bank.
+        </Box>
       </Box>
     </Container>
   );
