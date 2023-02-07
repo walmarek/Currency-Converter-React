580e2c28bf3b67e1323ecb6c0cc519c2a47180e2
Marek Wal <walmarek1987@gmail.com>
2023-02-07 11:08:44 +0100

    status message added (error, loading)

--- src/ConverterRow/index.js
+++ src/ConverterRow/index.js
@@ -1,13 +1,32 @@
+import useCurrencyOptions from "../useCurrencyOptions";
 import { Fieldset, Legend, Span } from "./styled";
 
-const ConverterRow = ({ input, output, button }) => (
-  <Fieldset>
-    <Legend>Currency converter</Legend>
-    <Span>Select currency:</Span>
-    {input}
-    {output}
-    {button}
-  </Fieldset>
-);
+const ConverterRow = ({ input, output, button }) => {
+  const {status} = useCurrencyOptions("loading")
+
+  return (
+    <Fieldset>
+      <Legend>Currency converter</Legend>
+      { status === "loading" ? (
+        <p>
+Loading...
+        </p>
+          
+      ) : status === "error" ? (
+        <p>
+          Ups... Something gone wrong! <br />
+          Plese check your internet conection and reload!
+        </p>
+      ) : (
+        <>
+          <Span>Select currency:</Span>
+          {input}
+          {output}
+          {button}
+        </>
+      )}
+    </Fieldset>
+  );
+}
 
 export default ConverterRow;
